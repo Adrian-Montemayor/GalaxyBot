3c5a980a25452f642fdfc5ec78cc81320a82087e
Adrian-Montemayor <adrianmoncar@hotmail.com>
2021-06-24 20:44:06 -0500

    remove: comments

--- galaxy_tracker.js
+++ galaxy_tracker.js
@@ -41,27 +41,3 @@ bot.on('ready', function(evt){
         }
     });
 });
-
-
-
-
-// bot.on('ready', function (evt) {
-//     logger.info('Connected');
-//     logger.info('Logged in as: ');
-//     logger.info(bot.username + ' - (' + bot.id + ')');
-// });
-//     bot.on('message', function (user, userID, channelID, message, evt){
-//     if(message.substring(0,1) == "!"){
-//         let args = message.substring(1).split(' ');
-//         let cmd = args[0];
-//         args = args.splice(1);
-//         switch(cmd){
-//             case 'intro':
-//                 bot.sendMessage({
-//                     to: channelID,
-//                     message: 'Greetings! welcome to the server'
-//                 });
-//             break;
-//         }
-//     }
-//     });
